bf096dd55e6544a374432dc26992f666ecc6c4f8
Jesse Liberty <jesseliberty@gmail.com>
2024-01-02 10:32:14 -0500

    first cars commit

---
+++ Data/Interfaces/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cars.Data.Entities;
+
+namespace Cars.Data.Interfaces
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAll(bool returnDeletedRecords = false);
+        Task<Car?> Get(int id);
+        Task<int> UpsertAsync(Car car);
+        Task<int> DeleteAsync(int id);
+    }
+}
