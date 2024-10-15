d9c8c3ddc9848b2ad5f65a1bea145d5759122f29
Joe Dluzen <jdluzen@gmail.com>
2024-10-15 19:30:36 -0400

    Fix chapter04 swagger gen.

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -1,6 +1,8 @@
 using Cars.Data;
 using Cars.Data.Interfaces;
 using Cars.Data.Repositories;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+ x =>
+ {
+     x.SwaggerDoc(
+         "v1",
+         new OpenApiInfo
+         {
+             Title = $"{Assembly.GetExecutingAssembly().GetName().Name}",
+             Version = "Version 1",
+             Description = "Create documentation for Cars",
+             Contact = new OpenApiContact
+             {
+                 Name = "Jesse Liberty",
+                 Email = "jesseliberty@gmail.com",
+                 Url = new Uri("https://jesseliberty.com")
+             }
+         });
+     var xmlFilename = System.IO.Path.Combine(System.AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+     x.IncludeXmlComments(xmlFilename);
+ });
 
 // Load DB configuration and register the connection factory for injection
 var configuration = builder.Configuration;
