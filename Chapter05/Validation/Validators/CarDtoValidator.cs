79f1263837d42722e3a1ef7a665215772b110079
Joe Dluzen <jdluzen@gmail.com>
2024-10-15 19:47:25 -0400

    Add cascade commeted code.

--- Chapter05/Validation/Validators/CarDtoValidator.cs
+++ Chapter05/Validation/Validators/CarDtoValidator.cs
@@ -10,6 +10,7 @@ namespace Cars.Validators
             //  RuleFor(x => x.Is_Deleted).Equal("0").WithMessage("Car must not be deleted");
             //RuleFor(x => x.Is_Deleted).NotEmpty().Equal("0").WithMessage("Car must not be deleted");
             //RuleFor(car => car.Is_Deleted).Must(isDeleted => isDeleted == "0").WithMessage("Car must have value zero");
+            //RuleFor(x => x.Is_Deleted).Cascade(CascadeMode.Stop).NotEmpty().Equal("0").WithMessage("Car must not be deleted");
             RuleFor(car => car.Is_Deleted).Must(isDeleted => isDeleted == "0").WithSeverity(Severity.Warning).WithMessage("Car must have value zero");
         }
     }
